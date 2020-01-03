df954afd09b00b37e2e0d30d2af45319039f3ff7
Rachel Iaconianni <iaconiannir@gmail.com>
2020-01-03 10:20:41 -0500

    added <textarea> and js vars

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// variable for the current day
+var currentDay = document.querySelector("#currentDay");
+// variable to make buttons save to local storage
+var saveBtn = document.querySelectorAll(button);
+// variable to make the hourly plans save
+var hourlyPlans = localStorage.getItem(".plans");
+
+// Add current day with moment.js to <p class="currentDay">
+
+// Create function to save hourly plans to local storage
